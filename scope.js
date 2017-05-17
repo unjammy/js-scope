e753199e5c6c2ea944077ec6d08e9f1576275b41
clarkdenunzio <clark@clarkdenunzio.com>
2017-05-17 13:34:39 -1000

    outer and inner

--- scope.js
+++ scope.js
@@ -19,7 +19,7 @@ function outerFunction() {
 
   function innerFunction() {
     var world = 'World';
-    return /* answer here */;
+    return hello + ' ' + world;/* answer here */;
   }
   return innerFunction();
 }
